d474db2b3da544bf37efb3dc85a8edd54718736a
Syed Haider <shayder426@gmail.com>
2020-06-10 22:32:41 -0700

    Able to read/write images as binary data

--- models/file.js
+++ models/file.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const fileSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   file: {
     type: Buffer,
     required: true,
