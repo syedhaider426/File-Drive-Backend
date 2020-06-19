cc56b964d93895d6af92ad31f21eb1879189ad38
Syed Haider <shayder426@gmail.com>
2020-06-19 12:16:10 -0700

    Validates user does not exist before registering

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,13 +17,18 @@ module.exports = function (app, db, passport) {
   });
   app.post("/api/register", checkNotAuthenticated, async (req, res) => {
     const collection = db.collection("users");
-    try {
-      const password = await bcrypt.hash(req.body.password, 10);
-      await collection.insertOne({ email: req.body.email, password: password });
-      res.redirect("/login");
-    } catch (err) {
-      res.status(404).json({ message: err });
-    }
+    const email = await collection.findOne({ email: req.body.email });
+    if (email)
+      return res
+        .status(404)
+        .json({ message: "User with that email already exists" });
+    const password = await bcrypt.hash(req.body.password, 10);
+    const result = await collection.insertOne({
+      email: req.body.email,
+      password: password,
+    });
+    if (!result) return res.status(404).json({ message: err });
+    return res.redirect("/login");
   });
   app.get("/login", checkNotAuthenticated, (req, res) => {
     res.send(`
@@ -41,8 +46,8 @@ module.exports = function (app, db, passport) {
     checkNotAuthenticated,
     passport.authenticate("local"),
     (req, res) => {
-      if (!req.user) res.redirect("/login");
-      res.redirect("/");
+      if (!req.user) return res.redirect("/login");
+      return res.redirect("/");
     }
   );
 };
